54783a007c3ffe63c33a6169f17fe27340ca6042
facu.almazan <facu.almazan@gmail.com>
2021-06-13 07:11:25 -0300

    editar comentario

--- pagina-comentarios.php
+++ pagina-comentarios.php
@@ -20,14 +20,16 @@
 		<li><a href="pagina-perfil.php"> Perfil </a></li>
 	</ul> 
 	<div class="content" '>
-		<div class="bloque" >
+		<div class="bloque" style="float:left; margin-left:10%" >
 			<h1 class="titulo"> Todos los comentarios </h1>
 <?php
 		$consulta= "SELECT * from comentarios";
 		$respuesta=consultar($consulta);
+		$id=$_SESSION['id'];
 		 if($respuesta){
 			foreach ($respuesta as $respuesta){
-				echo '<div class="prueba">';
+				if($respuesta['idUser']!=$id){
+					echo '<div class="prueba">';
 					$usuarioBuscar=$respuesta['idUser'];
 					$consulta2= "SELECT * from usuarios where id='$usuarioBuscar'";
 					$respuesta2=consultar($consulta2);
@@ -38,41 +40,46 @@
 					echo $nombre;
 					echo' dijo "';
 					echo $respuesta['comentario'];
-				    echo '"';
-				echo "</p></div>";
+					echo '"';
+					echo "</p></div>";
+				}
+				else{
+				$idComentario=$respuesta['id'];
+				$comentario=$respuesta['comentario'];
+				echo '<a class="btn" href=';
+				echo "'pagina-editarComentario.php?idComentario=$idComentario'";
+				echo '">';
+				echo '<p id="comentario"> Dijiste ';
+				echo '"';
+				echo $comentario;
+				echo '"';
+				echo '</p>';
+				echo "</a>";
+				}
+
 		    }
 		}
 	 ?>
 		</div>
-		<div class="bloque">
-		      <h1 class="titulo"> Tus comentarios </h1>
-			  <?php 
-			  $id=$_SESSION['id'];
-			  $consulta= "SELECT * from comentarios where idUser='$id'";
-	        $respuesta=consultar($consulta);
-		if($respuesta){ 
-			foreach ($respuesta as $respuesta){
+		<div class="bloque" style="float:right; margin-right:10%">
+			<h1 class="titulo"> Tus comentarios </h1>
+			<?php 
+			$id=$_SESSION['id'];
+			$consulta= "SELECT * from comentarios where idUser='$id'";
+	        $respuestas=consultar($consulta);
+		if($respuestas){ 
+			foreach ($respuestas as $respuesta){
 				$idComentario=$respuesta['id'];
-				echo '<div class="prueba">';
-				echo '<form method="POST" name="misComentarios" onsubmit="return checkComentario()" action="db-modificarComentario.php?id=';
-				echo $idComentario; echo'">';
-				echo '<h3></h3>
-					<input id="comentario" type="text" style="color:black" class="campoTexto" name="mensaje" value="';
-					echo $respuesta['comentario'];
-					echo '">';
-					echo '<br>';
-					echo '<input class="submit" type="submit" value="Guardar"</input>';
-				echo '</form>';
-				echo '<a class="comentarioEliminar" href="db-eliminarComentario.php?idComentario=';
-				echo $idComentario;
-				echo '"';
-				echo"> Eliminar </a>
-				</div>";
-		    	}
+				$comentario=$respuesta['comentario'];
+				echo '<button class="btn" onclick="location.href=';
+				echo "'pagina-editarComentario.php?idComentario=$idComentario'";
+				echo '">';
+				echo $comentario;
+				echo "</button>";
+		    }	
 		}
 			?>
 		</div>
 	</div>
-	<script src="scripts/script-comentario.js"></script> 
 </body>
 </html>
---
+++ pagina-editarComentario.php
@@ -0,0 +1,53 @@
+<?php
+	include_once "session.php";
+	$a = new autenticacion();
+	$a->validar_sesion();
+	$a->validar_rol(1);
+	$_SESSION['url']=3;
+    $id=$_SESSION['id'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap" rel="stylesheet">
+	<title>HOME USUARIO</title>
+		<link rel="stylesheet" type="text/css" href="estilo-comentarios.css">
+</head>
+<body>
+	 <ul>
+	 	<li><a class="logo" href='pagina-homeUsuario.php'><img src="img/avatar.png"></a></li>
+  		<li><a href="cerrarSesion.php" id="btn-cerrar">Salir </a></li>
+		<li><a href="pagina-perfil.php"> Perfil </a></li>
+	</ul> 
+	<div class="content">
+        <?php
+        if(isset($_GET['idComentario'])){
+		    $idComentario=$_GET['idComentario'];
+            $consulta = "SELECT comentario FROM comentarios WHERE id = $idComentario";
+            $respuesta=consultar($consulta);
+            foreach($respuesta as $respuesta){
+                $textoComentario=$respuesta['comentario'];
+            }
+            echo '<div class="prueba" style="height:auto; margin-top:10%">';
+				echo '<form id="coment" method="POST" onsubmit="return checkComentario()" action="db-modificarComentario.php?id=';
+				echo $idComentario; echo'">';
+				echo '<h3>Dijiste:</h3>
+					<input id="comentario" form="coment" type="text" style="color:black" class="campoTexto" name="mensaje" value="';
+					echo $textoComentario;
+					echo '">';
+					?>
+					<input type="submit" value="GUARDAR" class="modificar" onclick="return confirm('¿Seguro que quieres modificar?')">
+				</form>
+				<br>
+				<?php
+				echo '<a class="comentarioEliminar" href="db-eliminarComentario.php?idComentario=';
+				echo $idComentario;
+				echo '"';
+				echo"> Eliminar </a>
+				</div>";
+        }
+        ?>
+    </div>
+    <script  type="text/javascript" src="scripts/script-comentario.js"></script>
+</body>
