53391406f8bca96f74c6b40511c8b80a46f2596d
facu.almazan <facu.almazan@gmail.com>
2021-06-13 00:11:18 -0300

    fdggfddfgfgd

--- scripts/script-listar.js
+++ scripts/script-listar.js
@@ -2,4 +2,7 @@ window.onload = function(){
     if (window.location.hash == "#modificado"){
         alert("Se ha modificado con exito");
     }
+    if (window.location.hash == "#errorRepetidoRuta"){
+        alert("No se puede crear una ruta con los campos origen, destino y combi iguales a una existente");
+    }
 }
