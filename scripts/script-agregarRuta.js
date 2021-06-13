10a10987e1cd673541d09f570393f6165f43528e
facu.almazan <facu.almazan@gmail.com>
2021-06-13 00:20:26 -0300

    SOLUCIONAO

--- scripts/script-agregarRuta.js
+++ scripts/script-agregarRuta.js
@@ -1,84 +1,84 @@
-	var origen = document.getElementById("origen");
-	var destino = document.getElementById("destino");
-	var combi = document.getElementById("combi");
-	var km = document.getElementById("km");
-	var duracion = document.getElementById("duracion");
+var origen = document.getElementById("origen");
+var destino = document.getElementById("destino");
+var combi = document.getElementById("combi");
+var km = document.getElementById("km");
+var duracion = document.getElementById("duracion");
 
-	function checkRuta(){
-		return validarOrigen() && validarDestino() && validarRepeticion() && validarCombi() && validarKm() && validarDuracion();
+function checkRuta(){
+	return validarOrigen() && validarDestino() && validarRepeticion() && validarCombi() && validarKm() && validarDuracion();
+}
+
+function validarOrigen(){
+	if(origen.value =="x"){
+		alert("Selecciona un origen");
+		return false;
 	}
+	return true;
+}
 
-	function validarOrigen(){
-		if(origen.value =="x"){
-			alert("Selecciona un origen");
-			return false;
-		}
+function validarDestino(){
+	if(destino.value =="x"){
+		alert("Selecciona un destino");
+		return false;
+	}
+	return true;
+}
+
+function validarRepeticion(){
+	if(origen.value == destino.value){
+		alert("El origen y el destino no pueden ser iguales");
+		return false;
+	}
+	return true;
+}
+	
+function validarCombi(){
+	if(combi.value =="x"){
+		alert("Selecciona una combi");
+		return false;
+	}
+	return true;
+}
+
+function validarKm(){
+	if(km.value==""){
+		alert("Ingresa kilómetros");
+		return false;
+	}
+	return true;
+}
+
+function validarDuracion(){
+	if(duracion.value==""){
+		alert("Ingresa una duración");
+		return false;
+	}
+	if(!validarFormatoHora(duracion.value)){
+		alert("La duración debe cumplir con el formato HH:MM:SS");
+		return false;
+	}
+	return true;
+}
+
+function validarFormatoHora(Duracion){
+	var patron = /(?:[0-5]\d):(?:[0-5]\d):(?:[0-5]\d)/;
+	if (patron.test(Duracion)){
 		return true;
 	}
-
-	function validarDestino(){
-		if(destino.value =="x"){
-			alert("Selecciona un destino");
-			return false;
-		}
-		return true;
+	else{
+		return false;
 	}
-
-	function validarRepeticion(){
-		if(origen.value == destino.value){
-			alert("El origen y el destino no pueden ser iguales");
-			return false;
-		}
-		return true;
-	}
-		
-	function validarCombi(){
-		if(combi.value =="x"){
-			alert("Selecciona una combi");
-			return false;
-		}
-		return true;
-	}
-
-	function validarKm(){
-		if(km.value==""){
-			alert("Ingresa kilómetros");
-			return false;
-		}
-		return true;
-	}
-
-	function validarDuracion(){
-		if(duracion.value==""){
-			alert("Ingresa una duración");
-			return false;
-		}
-		if(!validarFormatoHora(duracion.value)){
-        	alert("La duración debe cumplir con el formato HH:MM:SS");
-			return false;
-		}
-		return true;
-	}
-
-	function validarFormatoHora(Duracion){
-		var patron = /(?:[0-5]\d):(?:[0-5]\d):(?:[0-5]\d)/;
-		if (patron.test(Duracion)){
-	        return true;
-	    }
-	    else{
-	        return false;
-	    }
-	}
+}
 
 
-	window.onload = function(){
-		if (window.location.hash == "#agregado"){
-			alert("La ruta se ha agregado con exito");
-		}
-		if (window.location.hash == "#error"){
-			alert("El origen y el destino no pueden ser el mismo");
-		}
-		if (window.location.hash == "#errorRepetido"){
-			alert("No se puede crear una ruta con los campos origen, destino y combi iguales a una existente");
-		}
+window.onload = function(){
+	if (window.location.hash == "#agregado"){
+		alert("La ruta se ha agregado con exito");
 	}
+	if (window.location.hash == "#error"){
+		alert("El origen y el destino no pueden ser el mismo");
+	}
+	if (window.location.hash == "#errorRepetido"){
+		alert("No se puede crear una ruta con los campos origen, destino y combi iguales a una existente");
+	}
+}
--- scripts/script-listar.js
+++ scripts/script-listar.js
@@ -5,16 +5,4 @@ window.onload = function(){
     if (window.location.hash == "#errorRepetidoRuta"){
         alert("No se puede crear una ruta con los campos origen, destino y combi iguales a una existente");
     }
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-    if (window.location.hash == "#errorOrigDest"){
-        alert("El origen y destino no pueden ser iguales");
-    }
->>>>>>> 43f80b9776cec460bd44cf2f9cad31356e4e8846
->>>>>>> 651da343a9db71102f8c2bec2760dfb3d762ec3b
->>>>>>> 4a6b223563cbd4ef358f73da9939f87c236e8d7c
 }
